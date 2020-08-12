8f4145500ad5573d85151f72b4484fa58141aba8
LiuZhongyi <46397760+codemonkey2019@users.noreply.github.com>
2020-08-12 21:24:53 +0800

    现在登录和注册时会使用服务器端的公钥进行加密通讯

--- src/test/java/com/clouddisk/server/ServerApplicationTests.java
+++ src/test/java/com/clouddisk/server/ServerApplicationTests.java
@@ -2,9 +2,15 @@ package com.clouddisk.server;
 
 import com.clouddisk.server.entity.User;
 import com.clouddisk.server.mapper.UserMapper;
+import com.clouddisk.server.util.FileUtils;
+import com.cryptotool.block.AE;
+import com.cryptotool.cipher.MyCipher;
+import com.cryptotool.cipher.MyCipherFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 @SpringBootTest
 class ServerApplicationTests {
@@ -15,5 +21,18 @@ class ServerApplicationTests {
         User user = new User(null,"lisi","123");
         System.out.println(userMapper.selectByUserNameAndPassword(user));
         System.out.println(userMapper.registUserByNamePassword(user));
+    }
+
+    @Test
+    public void test() throws IOException {
+//        AEKeyPair keyPair = KeyUtils.getAEKeyPair(AE.SM2);
+//        FileUtils.writeFile("SM2KeyPair\\ec.pkcs8.pri.der",keyPair.getPrivateKey());
+//        FileUtils.writeFile("SM2KeyPair\\ec.pkcs8.pri.der",keyPair.getPrivateKey());
+        MyCipher cipher= MyCipherFactory.getAECipher(AE.SM2,
+                FileUtils.readFile("SM2KeyPair\\ec.pkcs8.pri.der"),
+                FileUtils.readFile("SM2KeyPair\\ec.x509.pub.der"));
+        byte[] data = "hellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohello".getBytes();
+        byte[] encrypt = cipher.encrypt(data);
+        System.out.println(new String(cipher.decrypt(encrypt)));
     }
 }
